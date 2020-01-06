80bff7831dae55c3138ab46a3cbc38011971d6cd
Brendan Abel <007brendan@gmail.com>
2020-01-06 14:00:11 -0800

    Update for pypi distribution
    - Convert to package (from module)
    - Update to newer gitignore
    - Python3
    - Refactor setup.py

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-piglatin
-========
+# piglatin
 
-converts english text to piglatin
+Converts english text to piglatin.
+
+## Installation
+
+`piglatin` can be installed from PyPI using `pip`.
+
+    pip install piglatin
+
+## Usage
+
+```python
+>>> import piglatin
+>>> piglatin.translate("hello world!")
+'ello-hay orld-way!'
+```
+
+## Command Line Usage
+
+`piglatin` can also be used as a command line tool.
+
+```bash    
+$ python -m piglatin "hello world!"
+ello-hay orld-way
+```
+
+Or read and write to files.
+
+```bash
+$ python -m piglatin -i input.txt -o output.txt
+```
+
--- setup.py
+++ setup.py
@@ -1,14 +1,57 @@
-
 import sys
 import os
+import io
+from typing import List, Dict
 from setuptools import setup, find_packages
 
 
+# Package meta-data.
+NAME = "piglatin"
+DESCRIPTION = "Converts text to piglatin."
+URL = "https://github.com/bpabel/piglatin"
+EMAIL = "007brendan@gmail.com"
+AUTHOR = "Brendan Abel"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = None
+REQUIRED: List[str] = []
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about: Dict[str, str] = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, "_version.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+# Where the magic happens:
 setup(
-	name='piglatin',
-	version='0.1',
-	description='Converts text to piglatin.',
-	author='Brendan Abel',
-	author_email='007brendan@gmail.com',
-	py_modules=['piglatin'],
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
 )
